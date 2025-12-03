12b9eec817baee8013b2184158bc7f7b57bd6529
harshith-podifi <harshith.padma@podifi.ai>
2025-12-03 12:01:50 +0530

    ✨feat: add stock indicators for menu items and modifiers SMP-156

---
+++ src/db/migrations/0013_fancy_omega_red.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "menu_items" ADD COLUMN "is_available" boolean DEFAULT true NOT NULL;--> statement-breakpoint
+ALTER TABLE "modifier_options" ADD COLUMN "is_available" boolean DEFAULT true NOT NULL;--> statement-breakpoint
+CREATE INDEX "menu_items_is_available_index" ON "menu_items" USING btree ("is_available");--> statement-breakpoint
+CREATE INDEX "modifier_options_is_available_index" ON "modifier_options" USING btree ("is_available");
