1053a1a41c9401b7c01739864c998542434b0994
harshith-podifi <harshith.padma@podifi.ai>
2025-12-05 15:02:19 +0530

    🐛 fix: package-lock dependencies updated

--- src/db/migrations/0013_nice_wind_dancer.sql
+++ src/db/migrations/0013_nice_wind_dancer.sql
@@ -1,3 +1,4 @@
+ALTER TABLE "dine_in_restaurants" ADD COLUMN "business_hours" jsonb;--> statement-breakpoint
 ALTER TABLE "menu_items" ADD COLUMN "is_available" boolean DEFAULT true NOT NULL;--> statement-breakpoint
 ALTER TABLE "modifier_options" ADD COLUMN "is_available" boolean DEFAULT true NOT NULL;--> statement-breakpoint
 CREATE INDEX "menu_items_is_available_index" ON "menu_items" USING btree ("is_available");--> statement-breakpoint
